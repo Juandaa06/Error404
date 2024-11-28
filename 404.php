a5bfd8f24c8ef9d99d0f13c1833936a3ac5e54b4
Juan Cañas <126015180+Juandaa06@users.noreply.github.com>
2024-11-28 10:02:55 -0500

    Update 404.php

--- 404.php
+++ 404.php
@@ -1,32 +1,44 @@
-<div class="container-404">
-  <h1 class="first-four">4</h1>
-  <div class="cog-wheel1">
-      <div class="cog1">
-        <div class="top"></div>
-        <div class="down"></div>
-        <div class="left-top"></div>
-        <div class="left-down"></div>
-        <div class="right-top"></div>
-        <div class="right-down"></div>
-        <div class="left"></div>
-        <div class="right"></div>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container-404">
+    <h1 class="first-four">4</h1>
+    <div class="cog-wheel1">
+        <div class="cog1">
+            <div class="top"></div>
+            <div class="down"></div>
+            <div class="left-top"></div>
+            <div class="left-down"></div>
+            <div class="right-top"></div>
+            <div class="right-down"></div>
+            <div class="left"></div>
+            <div class="right"></div>
+        </div>
     </div>
-  </div>
-  
-  <div class="cog-wheel2"> 
-    <div class="cog2">
-        <div class="top"></div>
-        <div class="down"></div>
-        <div class="left-top"></div>
-        <div class="left-down"></div>
-        <div class="right-top"></div>
-        <div class="right-down"></div>
-        <div class="left"></div>
-        <div class="right"></div>
+    
+    <div class="cog-wheel2"> 
+        <div class="cog2">
+            <div class="top"></div>
+            <div class="down"></div>
+            <div class="left-top"></div>
+            <div class="left-down"></div>
+            <div class="right-top"></div>
+            <div class="right-down"></div>
+            <div class="left"></div>
+            <div class="right"></div>
+        </div>
     </div>
-  </div>
- <h1 class="second-four">4</h1>
- <p class="wrong-para">Lo sentimos, la página o recurso que estás buscando no está disponible.</p>
- <a class="btn btn-light error404" href="http://localhost/mesa_it/index.php?vista=home">Volver al menu de Inicio</a>
-</div>
-
+    <h1 class="second-four">4</h1>
+    <p class="wrong-para">Lo sentimos, la página o recurso que estás buscando no está disponible.</p>
+    <a class="btn btn-light error404" href="http://localhost/mesa_it/index.php?vista=home">Volver al menu de Inicio</a>
+    </div>
+</body>
+</html>
